ec6ce93d2a64885bee1f1822282110da8ee334f1
shailendra yadav <shailendra_yadav@shailendras-Mac-mini.local>
2025-05-31 15:33:10 +0530

    FJF-103: minor changes :

---
+++ forkjoin-demo-app/src/main/java/com/infoiv/demo/DemoApplication.java
@@ -0,0 +1,8 @@
+package com.infoiv.demo;
+
+public class DemoApplication {
+    // need to change to spring boot support
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
